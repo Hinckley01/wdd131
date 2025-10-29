fcc0cb618be524a19391c1e4ae84a4be171234e8
Hinckley01 <hohaegbulem@byupathway.edu>
2025-10-29 14:53:24 +0100

    changes made in images and index and scripts

--- week1/styles/scripts/getdates.js
+++ week1/styles/scripts/getdates.js
@@ -1,5 +1,12 @@
-const currentYear = new Date().getFullYear();
-
-document.getElementById('currentyear').textContent = currentYear;
-
-document.getElementById('lastModified').textContent = `Last modified: ${document.lastModified}`;
+document.addEventListener("DOMContentLoaded", () => {
+    const currentDateTime = document.getElementById("currentDateTime");
+    const lastModified = document.getElementById("lastModified");
+  
+    // Show current date and time
+    const now = new Date();
+    currentDateTime.textContent = now.toLocaleString();
+  
+    // Show last modified date of the document
+    lastModified.textContent = `Last Modified: ${document.lastModified}`;
+  });
+  
