a986c24d8006f651eba6cc162bf71b7c905832ec
Hinckley01 <hohaegbulem@byupathway.edu>
2025-11-07 12:57:02 +0100

    changes were made in images in the css and in the html

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,22 +1,4 @@
-// ---------- Footer Dynamic Date ----------
 document.addEventListener("DOMContentLoaded", () => {
-    // Current Year
-    const currentYear = new Date().getFullYear();
-    document.getElementById("current year").textContent = currentYear;
-  
-    // Last Modified Date
-    const lastModified = document.lastModified;
-    document.getElementById("lastModified").innerHTML = `
-      <span class="highlight">Last Modification:</span> ${lastModified}
-    `;
-  });
-  
-  // ---------- Responsive Navigation ----------
-  const menuButton = document.querySelector("#menu");
-  const navList = document.querySelector("nav ul");
-  
-  menuButton.addEventListener("click", () => {
-    navList.classList.toggle("open");
-    menuButton.classList.toggle("active"); // toggle animation class
-  });
-  
+  document.getElementById("currentYear").textContent = new Date().getFullYear();
+  document.getElementById("lastModified").innerHTML += ` ${document.lastModified}`;
+});
